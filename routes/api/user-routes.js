08cd82c293f5f23c85054656dedc880d18d792c8
shhu21 <hu.sara21@gmail.com>
2020-08-11 13:32:54 -0700

    adds pwd encryption

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -93,4 +93,15 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+    }
+})
+
 module.exports = router;
