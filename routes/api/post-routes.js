1c244db2c7c3a01afedf03b8aa72babd97f8a57b
shhu21 <hu.sara21@gmail.com>
2020-08-18 14:50:47 -0700

    adds comment feature

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,24 +1,34 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [['created_at', 'DESC']], 
+        order: [['created_at', 'DESC']],
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
           {
             model: User,
             attributes: ['username']
           }
         ]
     })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-    });
 });
 
 router.get('/:id', (req, res) => {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -22,17 +22,26 @@ router.get('/:id', (req, res) => {
           id: req.params.id
         },
         include: [
-            {
+          {
+            model: Post,
+            attributes: ['id', 'title', 'post_url', 'created_at']
+          },
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
               model: Post,
-              attributes: ['id', 'title', 'post_url', 'created_at']
-            },
-            {
-              model: Post,
-              attributes: ['title'],
-              through: Vote,
-              as: 'voted_posts'
+              attributes: ['title']
             }
-          ]
+          },
+          {
+            model: Post,
+            attributes: ['title'],
+            through: Vote,
+            as: 'voted_posts'
+          }
+        ]
     })
     .then(dbUserData => {
         if (!dbUserData) {
